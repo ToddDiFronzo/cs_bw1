dd2bbd96d319ec9a631ea279edb7e14a4fa32e98
Todd DiFronzo <fonze@aol.com>
2020-08-27 22:30:23 -0400

    what i have so far

--- conway.py
+++ conway.py
@@ -1,30 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
 
-
+N = 100
 ON = 255
 OFF = 0
 vals = [ON, OFF]
 
-def myGrid(N):
-    # returns a grid of N*N random values
-    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+# grid of N*N random values
+grid = np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N,N)
 
-def rules(grid, N):
-    newGrid = grid.copy()
+ugrid = grid.copy()
+
+def grid_update(data):
+    global grid 
+
     for i in range(N):
         for j in range(N):
-            #  8 neighbors
-            
+        # 8 neighbor sum 
             total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
-                         grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
-                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
-                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+                        grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                        grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                        grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
 
             # Conway's rules
             if grid[i, j]  == ON:
                 if (total < 2) or (total > 3):
-                    newGrid[i, j] = OFF
+                    ugrid[i, j] = OFF
             else:
                 if total == 3:
-                    newGrid[i, j] = ON
+                    ugrid[i, j] = ON
+
+    # data update
+    mat.set_data(ugrid)
+    grid = ugrid
+    return [mat]
+
+# basic animation
+fig, ax = plt.subplots()
+mat = ax.matshow(grid)
+ani = animation.FuncAnimation(fig, grid_update, interval=50,
+                              save_count=50)
+plt.show()
